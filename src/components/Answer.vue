fc2a5d0571407302839ca8587ad3516eecf89eb1
Rasmus <rasmusjons@gmail.com>
2020-01-19 08:11:13 +0100

    animations fixed

--- src/components/Answer.vue
+++ src/components/Answer.vue
@@ -1,42 +1,44 @@
 <template>
     <div>
         <ul>
-            <li v-for="(answer, index) in answers" @click="display(index)">{{answer}}</li>
+            <li v-for="(answer, index) in answers" @click="display(index)">
+                {{ answer }}
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
-export default {
-    props: ["answers", "numberA", "numberB"],
+    export default {
+        props: ['answers', 'numberA', 'numberB'],
 
-    methods: {
-        display(index) {
-            this.$emit("clickedAnswer", index);
-        }
-    }
-};
+        methods: {
+            display(index) {
+                this.$emit('clickedAnswer', index);
+            },
+        },
+    };
 </script>
 
 <style scoped>
-ul {
-    column-count: 2;
-    width: 600px;
-    height: 200px;
-    padding: 10px;
-    margin: auto;
-    display: inline-block;
-}
+    ul {
+        column-count: 2;
+        width: 600px;
+        height: 100%;
 
-li {
-    text-align: center;
-    display: list-item;
-    margin-left: 30%;
-    background-color: #374dc5;
-    color: white;
-    padding: 15px;
-    width: 20%;
-    border-radius: 25%;
-    margin-bottom: 10%;
-}
+        padding: 10px;
+        display: inline-block;
+    }
+
+    li {
+        text-align: center;
+        display: list-item;
+        margin-left: 30%;
+        background-color: #374dc5;
+        color: white;
+        padding: 15px;
+        width: 20%;
+        border-radius: 25%;
+        margin-bottom: 30px;
+    }
 </style>
--- src/components/Correct.vue
+++ src/components/Correct.vue
@@ -2,38 +2,41 @@
     <div class="success">
         <div>That's Correct!</div>
         <hr />
-        <button class="btn btn-primary" @click="newQuestion()">Next Question</button>
+        <button class="btn btn-primary" @click="newQuestion()">
+            Next Question
+        </button>
     </div>
 </template>
 
 <script>
-export default {
-    methods: {
-        newQuestion() {
-            console.log("newQ");
-            this.$emit("newQuestion", "hej");
-        }
-    }
-};
+    export default {
+        methods: {
+            newQuestion() {
+                console.log('newQ');
+                this.$emit('newQuestion', 'hej');
+            },
+        },
+    };
 </script>
 
 <style>
-.success {
-    font: 1000;
-    font-size: 50px;
-    text-align: center;
-    width: 600px;
-    height: 200px;
-    margin: auto;
-    color: #3c763d;
-    background-color: #dff0d8;
-    border-color: #d6e9c6;
-    padding: 15px;
-    margin-bottom: 20px;
-    border: 1px solid transparent;
-    border-radius: 4px;
-}
-.success hr {
-    border: solid lightgray 0.5px;
-}
+    .success {
+        font: 1000;
+        font-size: 50px;
+        text-align: center;
+        width: 600px;
+        height: 200px;
+
+        margin: auto;
+        color: #3c763d;
+        background-color: #dff0d8;
+        border-color: #d6e9c6;
+        padding: 15px;
+        border: 1px solid transparent;
+        border-radius: 4px;
+        margin-bottom: 30px;
+    }
+    .success hr {
+        border: solid lightgray 0.5px;
+    }
 </style>
